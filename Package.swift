a9d9528374fefbab39247b8f864c1ef48296f518
Helge Heß <devteam@zeezide.de>
2018-03-16 17:46:07 +0100

    Use iOS fixed package
    See: https://github.com/apple/swift-nio/pull/180

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         .package(url: "https://github.com/AlwaysRightInstitute/cows.git",
                  from: "1.0.0"),
         */
-        .package(url: "https://github.com/apple/swift-nio.git", 
-                 from: "1.2.1"),
+        .package(url: "https://github.com/helje5/swift-nio.git",
+                 .branch("bugz/platform-consistency")),
     ],
 
     targets: [
