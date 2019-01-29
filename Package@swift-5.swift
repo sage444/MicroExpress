d6a8f4494cf13f4ac339f4fbc704e58d2936b0f2
Helge Heß <devteam@zeezide.de>
2019-01-29 17:31:51 +0100

    Add a Swift 5 manifest
    Doesn't work w/ Xcode 10.2b1 though, misses `Result`.

---
+++ Package@swift-5.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:5.0
+//
+//  Package.swift
+//  MicroExpress
+//
+//  Created by Helge Hess on 09.03.18.
+//  Copyright © 2018-2019 ZeeZide. All rights reserved.
+//
+import PackageDescription
+
+let package = Package(
+    name: "MicroExpress",
+    
+    products: [
+        .library(name: "MicroExpress", targets: ["MicroExpress"]),
+    ],
+    
+    dependencies: [
+        /* Add your package dependencies in here
+        .package(url: "https://github.com/AlwaysRightInstitute/cows.git",
+                 from: "1.0.0"),
+        */
+        .package(url: "https://github.com/apple/swift-nio.git", 
+                 .branch("master")),
+        .package(url: "https://github.com/AlwaysRightInstitute/mustache.git",
+                 from: "0.5.6")
+    ],
+
+    targets: [
+        .target(name: "MicroExpress", 
+                dependencies: [
+                    /* Add your target dependencies in here, e.g.: */
+                    // "cows",
+                    "NIO",
+                    "NIOHTTP1",
+                    "mustache"
+                ])
+    ]
+)
